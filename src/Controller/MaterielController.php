9eb8a0a74f3e912367c677e2a08c257d23652cfa
galexand <giordana.alex@gmail.com>
2024-01-18 15:20:27 +0100

    add cache and invalidatetag

--- src/Controller/MaterielController.php
+++ src/Controller/MaterielController.php
@@ -2,20 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Materiel;
-use App\Repository\MaterielRepository;
 use DateTimeImmutable;
+use App\Entity\Materiel;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Expr\Func;
+use App\Repository\MaterielRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\cache;
 
 class MaterielController extends AbstractController
 {
@@ -29,10 +33,17 @@ class MaterielController extends AbstractController
     }
 
     #[Route('/api/materiel', name: 'app_materiel')]
-    public function getAllMateriel(MaterielRepository $materielRepository,SerializerInterface $serializer): JsonResponse
+    public function getAllMateriel(MaterielRepository $materielRepository,SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
         $materiels = $materielRepository->findBy(['status' => 'En stock']);
         $jsonMateriels = $serializer->serialize($materiels,'json', ['groups' => 'get_materiel']);
+        $idCache = "getAllMateriel";
+        $jsonMateriels = $cache->get($idCache, function(ItemInterface $item) use ($materielRepository, $serializer){
+            $item->tag("materielCache");
+            echo 'mise en cache';
+            $materielsList = $materielRepository->findBy(['status' => 'En stock']);
+            return $serializer->serialize($materielsList, 'json', ['groups' => 'get_materiel']);
+        });
         return new JsonResponse($jsonMateriels,Response::HTTP_OK,[],true);
     }
 
@@ -46,13 +57,14 @@ class MaterielController extends AbstractController
     }
 
     #[Route('api/materiel/update/{id}', name: 'app_materiel_update', methods: ['PUT','PATCH'])]
-    public function updateMateriel(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function updateMateriel(TagAwareCacheInterface $cache, MaterielRepository $materielRepository,SerializerInterface $serializer, $id, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $materiel = $materielRepository->find($id);
         $data = json_decode($request->getContent(),true);
         empty($data['name']) ? true : $materiel->setName($data['name']);
         empty($data['available']) ? true : $materiel->setAvailable($data['available']);
         $materiel->setUpdatedAt(new DateTimeImmutable());
+        $cache->invalidateTags(["materielCache"]);
         $entityManager->persist($materiel);
         $entityManager->flush();
         $jsonMateriel = $serializer->serialize($materiel,'json', ['groups' => 'get_materiel']);
@@ -60,9 +72,10 @@ class MaterielController extends AbstractController
     }
 
     #[Route('api/materiel/harddelete/{id}', name: 'app_materiel_delete', methods: ['DELETE'])]
-    public function deleteMateriel(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager): JsonResponse
+    public function deleteMateriel(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $materiel = $materielRepository->find($id);
+        $cache->invalidateTags(["materielCache"]);
         $entityManager->remove($materiel);
         $entityManager->flush();
         $jsonMateriel = $serializer->serialize($materiel,'json');
@@ -70,11 +83,12 @@ class MaterielController extends AbstractController
     }
 
     #[Route('api/materiel/delete/{id}', name: 'app_materiel_soft_delete')]
-    public function softDelete(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager): JsonResponse
+    public function softDelete(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $materiel = $materielRepository->find($id);
         $materiel->setStatus("off");
         $materiel->setUpdatedAt(new DateTimeImmutable());
+        $cache->invalidateTags(["materielCache"]);
         $entityManager->persist($materiel);
         $entityManager->flush();
         $jsonMateriel = $serializer->serialize($materiel,'json', ['groups' => 'delete_materiel']);
@@ -82,11 +96,12 @@ class MaterielController extends AbstractController
     }
 
     #[Route('api/materiel/restore/{id}', name: 'app_materiel_restore')]
-    public function restore(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager): JsonResponse
+    public function restore(MaterielRepository $materielRepository,SerializerInterface $serializer, $id, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $materiel = $materielRepository->find($id);
         $materiel->setStatus("en stock");
         $materiel->setUpdatedAt(new DateTimeImmutable());
+        $cache->invalidateTags(["materielCache"]);
         $entityManager->persist($materiel);
         $entityManager->flush();
         $jsonMateriel = $serializer->serialize($materiel,'json', ['groups' => 'delete_materiel']);
@@ -94,7 +109,7 @@ class MaterielController extends AbstractController
     }
 
     #[Route('api/materiel/add', name: 'app_materiel_add', methods: ['POST'])]
-    public function addMateriel(SerializerInterface $serializer, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function addMateriel(SerializerInterface $serializer, Request $request, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $materiel = new Materiel();
         $data = json_decode($request->getContent(),true);
@@ -104,6 +119,7 @@ class MaterielController extends AbstractController
         $materiel->setStatus("En stock");
         $materiel->setCreatedAt(new DateTimeImmutable());
         $materiel->setUpdatedAt(new DateTimeImmutable());
+        $cache->invalidateTags(["materielCache"]);
         $entityManager->persist($materiel);
         $entityManager->flush();
         $jsonMateriel = $serializer->serialize($materiel,'json');
