224b5d1b37f5fe67a6c59037a6448ac4ec431b25
galexand <alexandre.giordana. aragon2@gmail.com>
2024-01-31 11:34:01 +0100

    adding nelmio documentation for download files

--- src/Controller/DownloadedFilesController.php
+++ src/Controller/DownloadedFilesController.php
@@ -21,9 +21,21 @@ class DownloadedFilesController extends AbstractController
 
         
     }
-     #[Route('/api/file', name: 'files.create', methods:["POST"])]
+    /**
+     * Crée un nouveau fichier téléchargé.
+     *
+     * @Route("/api/file", name="files.create", methods={"POST"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @param UrlGeneratorInterface $urlGenerator
+     * @return JsonResponse
+     */
+    #[Route('/api/file', name: 'files.create', methods:["POST"])]
     public function createFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, 
-    EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
+        EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
         $newFile = new DownloadedFiles();
 
@@ -49,6 +61,16 @@ class DownloadedFilesController extends AbstractController
         $location = $urlGenerator->generate("app.index",[], UrlGeneratorInterface::ABSOLUTE_URL);
         return new JsonResponse($jsonPicture, Response::HTTP_CREATED,["Location"=> $location. $realpath . "/".$slug]);
     }
+
+    /**
+     * Liste les fichiers téléchargés.
+     *
+     * @Route("/api/file", name="files.list", methods={"GET"})
+     *
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/file', name: 'files.list', methods:["GET"])]
     public function listFiles(DownloadedFilesRepository $repository, SerializerInterface $serializer): JsonResponse
     {
@@ -56,6 +78,18 @@ class DownloadedFilesController extends AbstractController
         $jsonFiles = $serializer->serialize($files, "json");
         return new JsonResponse($jsonFiles, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Affiche un fichier téléchargé.
+     *
+     * @Route("/api/file/{id}", name="files.show", methods={"GET"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param $id
+     * @return JsonResponse
+     */
     #[Route('/api/file/{id}', name: 'files.show', methods:["GET"])]
     public function showFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, $id): JsonResponse
     {
@@ -63,6 +97,19 @@ class DownloadedFilesController extends AbstractController
         $jsonFile = $serializer->serialize($file, "json");
         return new JsonResponse($jsonFile, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Modifie un fichier téléchargé.
+     *
+     * @Route("/api/file/{id}", name="files.update", methods={"PUT", "PATCH"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @param $id
+     * @return JsonResponse
+     */
     #[Route('/api/file/{id}', name: 'files.update', methods:["PUT", "PATCH"])]
     public function updateFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, EntityManagerInterface $entityManager, $id): JsonResponse
     {
@@ -76,6 +123,19 @@ class DownloadedFilesController extends AbstractController
         $jsonFile = $serializer->serialize($file, "json");
         return new JsonResponse($jsonFile, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Supprime un fichier téléchargé.
+     *
+     * @Route("/api/file/{id}", name="files.delete", methods={"DELETE"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @param $id
+     * @return JsonResponse
+     */
     #[Route('/api/file/delete/{id}', name: 'files.hide', methods:["PUT", "PATCH"])]
     public function hideFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, EntityManagerInterface $entityManager, $id): JsonResponse
     {
@@ -86,6 +146,19 @@ class DownloadedFilesController extends AbstractController
         $jsonFile = $serializer->serialize($file, "json");
         return new JsonResponse($jsonFile, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Supprime un fichier téléchargé.
+     *
+     * @Route("/api/file/{id}", name="files.delete", methods={"DELETE"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @param $id
+     * @return JsonResponse
+     */
     #[Route('/api/file/{id}', name: 'files.delete', methods:["DELETE"])]
     public function deleteFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, EntityManagerInterface $entityManager, $id): JsonResponse
     {
@@ -95,6 +168,19 @@ class DownloadedFilesController extends AbstractController
         $jsonFile = $serializer->serialize($file, "json");
         return new JsonResponse($jsonFile, Response::HTTP_OK, [], true);
     }
+
+    /**
+     * Restaure un fichier téléchargé.
+     *
+     * @Route("/api/file/restore/{id}", name="files.restore", methods={"PUT", "PATCH"})
+     *
+     * @param Request $request
+     * @param DownloadedFilesRepository $repository
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @param $id
+     * @return JsonResponse
+     */
     #[Route('api/file/restore/{id}', name: 'files.restore', methods:["PUT", "PATCH"])]
     public function restoreFile(Request $request, DownloadedFilesRepository $repository, SerializerInterface $serializer, EntityManagerInterface $entityManager, $id): JsonResponse
     {
